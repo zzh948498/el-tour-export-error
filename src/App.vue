df9eb8ad813348d3103d4a2ccd83eceb6083cda8
zzh <zhang948498@163.com>
2024-04-23 09:53:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+</template>
+<script setup lang="ts">
+import { tourStepEmits } from 'element-plus/es/components/tour/src/step';
+console.log(tourStepEmits);
+</script>
